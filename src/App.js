ab12515bf3511998a0bae2f83871c121a94b380d
Vusal <vusalcs54@gmail.com>
2024-08-17 23:11:00 +0400

    Update index and App, add Post and Header

--- src/App.js
+++ src/App.js
@@ -1,55 +1,23 @@
 import './App.css';
+import Header from './Header';
+import Post from './Post';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Vusal Abdulkarimov</a>
-            <time>2024-08-16 23:00</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Vusal Abdulkarimov</a>
-            <time>2024-08-16 23:00</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Vusal Abdulkarimov</a>
-            <time>2024-08-16 23:00</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route index element={
+            <main>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+      } />
+      <Route path={"/login"} element={
+        <div>login</div>
+      } />
+    </Routes>
   );
 }
 
