d58b5699fdd69d00f8a17f9fe4aaa4387ee699e5
Vusal <vusalcs54@gmail.com>
2024-08-20 04:35:20 +0400

    Add CreatePost and UserContext, update multiple components and dependencies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import IndexPage from './pages/İndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route index element={<IndexPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,19 @@
-export default function Post() {
-  return(
+import { formatISO9075 } from 'date-fns';
+
+export default function Post({ title, summary, cover, content, createdAt, author}) {
+  return (
     <div className="post">
-    <div className="image">
-      <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" alt=""/>
+      <div className="image">
+      <img src={`http://localhost:4000/${cover}`} alt={title} />
+      </div>
+      <div className="texts">
+        <h2>{title}</h2>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
     </div>
-    <div className="texts">
-      <h2>Full-house battery backup coming later this year</h2>
-      <p className="info">
-        <a className="author">Vusal Abdulkarimov</a>
-        <time>2024-08-16 23:00</time>
-      </p>
-      <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-    </div>
-  </div>
   );
 }
